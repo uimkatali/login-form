4c09bdb5204cde2515af59718415989bb85c94b7
uimkatali <b8bf6b5@gmail.com>
2023-10-06 11:11:30 +0300

    simple changes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,12 @@
-'use client';
+"use client";
 
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
+import { getHome } from "@/app/api/general";
 
 export default function About() {
-  
   return (
     <main>
-        <h2>About</h2>
+      <h2></h2>
     </main>
-  )
+  );
 }
