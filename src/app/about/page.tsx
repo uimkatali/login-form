21b1a46a86009c8e4e7ace6dc6fe48f8d7bad647
uimkatali <b8bf6b5@gmail.com>
2023-10-06 10:23:56 +0300

    modify files

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+'use client';
+
+import React, { useEffect, useState } from 'react'
 
 export default function About() {
+  
   return (
     <main>
-        <h2>About haha</h2>
+        <h2>About</h2>
     </main>
   )
 }
